10355b3cc68fc0c85ffdedc048bb9b21afbe0970
Eugene Kubesh <codbasters@gmail.com>
2024-03-01 22:41:14 +0100

    v13.0.1

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
-    FsMapModule,
+    FsMapModule.forRoot(),
     BrowserAnimationsModule,
     AppMaterialModule,
     FormsModule,
