220c84867e22785c16f1f72bef39cfeb9696a83c
Raymond Gigliotti <ray@firestitch.com>
2024-05-14 12:48:57 -0400

    export interfaces

--- src/public_api.ts
+++ src/public_api.ts
@@ -4,6 +4,6 @@ export { FsMapComponent } from './app/components';
 export { FsMapLinkDirective, FsMapMarkerDirective } from './app/directives';
 export { FsMapModule } from './app/fs-map.module';
 export { FS_MAP_GOOGLE_MAP_KEY } from './app/injectors';
-export { MapAddress } from './app/interfaces/map-address.interface';
+export * from './app/interfaces';
 export { FsMap } from './app/services';
 
