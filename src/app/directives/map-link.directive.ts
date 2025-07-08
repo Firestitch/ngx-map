abe4788083b2f1f7c427e53e7747eb4ffb0bed6b
Raymond Gigliotti <ray@firestitch.com>
2025-07-08 17:01:49 -0400

    added lat lng to fsMapLink

--- src/app/directives/map-link.directive.ts
+++ src/app/directives/map-link.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+
 import { toAddress } from '../helpers';
 import { MapAddress } from '../interfaces';
 
@@ -10,16 +11,27 @@ export class FsMapLinkDirective implements OnChanges {
 
   @Input() public target = '_blank';
   @Input() public address: string | MapAddress;
+  @Input() public lat: number;
+  @Input() public lng: number;
 
-  public constructor(
+  constructor(
     private _el: ElementRef,
   ) { }
 
   public ngOnChanges(changes: SimpleChanges): void {
-    if (changes.address || changes.target) {
-      const href = `https://www.google.com/maps/search/?api=1&query=${toAddress(this.address)}`;
-      this._el.nativeElement.setAttribute('href', href);
-      this._el.nativeElement.setAttribute('target', this.target);
+    if (changes.address || changes.target || changes.lng || changes.lat) {
+
+      let href;
+      if(this.lng && this.lat) {
+        href = `https://www.google.com/maps/search/?api=1&query=${this.lat},${this.lng}`;
+      } else if(this.address) {
+        href = `https://www.google.com/maps/search/?api=1&query=${toAddress(this.address)}`;
+      }
+
+      if(href) {
+        this._el.nativeElement.setAttribute('href', href);
+        this._el.nativeElement.setAttribute('target', this.target);
+      }
     }
   }
 }
